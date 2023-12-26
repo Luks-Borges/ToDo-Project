388bf32ad693edabf9da877faf21f5c1de1ba650
Luks-Borges <l.borges.sym618@gmail.com>
2023-12-26 02:25:15 -0300

    Implementando Layout de User edit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   # GET /tasks or /tasks.json
   def index
     @tasks = Task.all
+    render layout: "all_tasks_layout"
   end
 
   # GET /tasks/1 or /tasks/1.json

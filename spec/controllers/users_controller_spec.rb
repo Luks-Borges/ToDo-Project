2fee6872649dee109bda98f3c90d726413a5875e
Lucas Borges <l.borges.sym618@gmail.com>
2024-01-18 15:08:11 -0300

    Mudanças para aceitar gif

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,13 @@
+require 'rails_helper'
+
 RSpec.describe UsersController, type: :controller do
     
     describe '#edit' do
-    it 'quando a requisição de edit é bem sucedida' do
-        get :edit, params: {id: task.id}
-        expect(user.name).to eq(user.name)
-        expect(response).to have_http_status(200)
-        expect(response).to render_template(:user_edit_layout)
+        it 'quando a requisição de edit é bem sucedida' do
+            get :edit, params: {id: task.id}
+            expect(user.name).to eq(user.name)
+            expect(response).to have_http_status(200)
+            expect(response).to render_template(:user_edit_layout)
+        end
     end
 end
